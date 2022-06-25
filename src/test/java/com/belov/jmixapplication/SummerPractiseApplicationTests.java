6b885cab6a96d6120d771a11b4c6835d74203b50
Vladislav Belov <vladik-belov-2015@mail.ru>
2022-06-25 18:52:29 +0400

    Initial commit of the projecte

---
+++ src/test/java/com/belov/jmixapplication/SummerPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belov.jmixapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerPractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
